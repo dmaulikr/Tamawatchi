373aad38b1cf5032a1ae272deb48cdce80c5760a
Morgan Steffy <morgansteffy2@gmail.com>
2016-03-18 16:05:47 -0500

    check if user exists before creating new account, segue to choose animal

---
+++ Tamawatchi/ChooseAnimalViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tamawatchi
+//
+//  Created by Morgan Steffy on 3/15/16.
+//  Copyright © 2016 Morgan Steffy. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ChooseAnimalViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- Tamawatchi/LoginViewController.swift
+++ Tamawatchi/LoginViewController.swift
@@ -23,6 +23,11 @@ class LoginViewController: UIViewController {
         loginButton.addTarget(self, action: "loginPressed", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(loginButton)
         
+        
+        if((NSUserDefaults.standardUserDefaults().objectForKey("currentUser")?.exists()) != nil){
+            let viewController = self.storyboard!.instantiateViewControllerWithIdentifier("homeVC") as UIViewController
+            self.presentViewController(viewController, animated: true, completion: nil)
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,7 +38,6 @@ class LoginViewController: UIViewController {
     func loginPressed(){
         
             let ref = Firebase(url: "https://brilliant-fire-4695.firebaseio.com")
-            
             let facebookLogin = FBSDKLoginManager()
             facebookLogin.logInWithReadPermissions(["public_profile"],  fromViewController: self, handler: {
                 (facebookResult, facebookError) -> Void in
@@ -43,25 +47,65 @@ class LoginViewController: UIViewController {
                 } else if facebookResult.isCancelled {
                     print("Facebook login was cancelled.")
                 } else {
-                    
-                        let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
-                        ref.authWithOAuthProvider("facebook", token: accessToken,
-                            withCompletionBlock: { error, authData in
-                                if error != nil {
-                                    print("Login failed. \(error)")
-                                } else {
-                                    print("Logged in! \(authData)")
-                                    
-                                    
-                       
-                                    
-                                            let newUser: NSDictionary = ["provider": authData.provider, "displayName": authData.valueForKeyPath("providerData.displayName")! ]
-                                            ref.childByAppendingPath("users").childByAppendingPath(authData.uid).setValue(newUser)
-                              
-                                }
-                        })
+                    let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                    ref.authWithOAuthProvider("facebook", token: accessToken,
+                        withCompletionBlock: { error, authData in
+                            if error != nil {
+                                print("Login failed. \(error)")
+                            } else {
+                                print("Logged in! \(authData)")
+                                
+                                //check if user already exists
+                                ref.childByAppendingPath("users/\(authData.uid)").observeEventType(.Value, withBlock: { snapshot in
+                                    print("snapshot is: \(snapshot)")
+                                    if(!snapshot.exists()){
+                                        print("made new user")
+                                        let newUser: NSDictionary = ["provider": authData.provider, "displayName": authData.valueForKeyPath("providerData.displayName")! ]
+                                        
+                                        ref.childByAppendingPath("users").childByAppendingPath(authData.uid).setValue(newUser)
+                                        
+                                        NSUserDefaults.standardUserDefaults().setObject(authData, forKey: "currentUser")
+                                        self.performSegueWithIdentifier("chooseAnimal", sender: self)
+                                    }
+                                    else{
+                                        print("snapshot has child: \(snapshot.hasChild("currentPet")) and the path is: \(snapshot.childSnapshotForPath("currentPet"))")
+                                            
+                                        if(snapshot.hasChild("currentPet") && snapshot.childSnapshotForPath("currentPet") != "none"){
+                                            
+                                            let viewController = self.storyboard!.instantiateViewControllerWithIdentifier("homeVC") as UIViewController
+                                            self.presentViewController(viewController, animated: true, completion: nil)
+                                        }
+                                        else{
+                                            print("segue to choose animal")
+                                           
+                                            let viewController = self.storyboard!.instantiateViewControllerWithIdentifier("chooseAnimalVC") as UIViewController
+                                            self.presentViewController(viewController, animated: true, completion: nil)
+                                        }
+                                    }
+                                })
+                            }
+                    })
                 }
             })
+        
+           }
+    
+   
+    func topMostController()-> UIViewController {
+        var topController = UIApplication.sharedApplication().keyWindow?.rootViewController
+        
+        while((topController?.presentedViewController) != nil){
+            topController = topController?.presentedViewController
+        }
+        if(topController != nil){
+            return topController!
+        }
+        else{
+            return self
+        }
+        
     }
+
+    
     
 }
